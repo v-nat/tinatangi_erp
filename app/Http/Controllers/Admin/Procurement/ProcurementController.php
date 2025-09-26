3ef21370568f851e7a297b71088a5e9d698ef1b0
Nathaniel <nathanielvasquez329@gmail.com>
2025-09-27 00:39:14 +0800

    front-end purchaseorder done

---
+++ app/Http/Controllers/Admin/Procurement/ProcurementController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Procurement;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProcurementController extends Controller
+{
+    //
+    public function index(){
+        return view("pages.admin.procurement.index");
+    }
+
+    public function createPO(){
+        return view("pages.admin.procurement.create-purchase-order");
+    }
+    public function supplier(){
+        return view("pages.admin.procurement.manage-supplier");
+    }
+    public function purchaseOrders(){
+        return view("pages.admin.procurement.purchase-orders");
+    }
+}
--- routes/procurement.php
+++ routes/procurement.php
@@ -1,16 +1,24 @@
 <?php
 
+use App\Http\Controllers\Admin\Procurement\ProcurementController;
 use App\Http\Controllers\Admin\Procurement\PurchaseOrderController;
 use App\Http\Controllers\Admin\Procurement\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/procurement', [PurchaseOrderController::class, 'index'])->name('procurement.index');
-    Route::get('/procurement/create-purchase-order', [PurchaseOrderController::class, 'createPO'])->name('procurement.createPO');
-    Route::get('/procurement/supplier', [PurchaseOrderController::class, 'supplier'])->name('procurement.supplier');
-    Route::get('/procurement/purchase-orders', [PurchaseOrderController::class, 'purchaseOrders'])->name('procurement.purchaseOrders');
+    //////////////////////////////////////////  to views /////////////////////////////////////
+    Route::get('/procurement', [ProcurementController::class, 'index'])->name('procurement.index');
+    Route::get('/procurement/create-purchase-order', [ProcurementController::class, 'createPO'])->name('procurement.createPO');
+    Route::get('/procurement/purchase-orders', [ProcurementController::class, 'purchaseOrders'])->name('procurement.purchaseOrders');
+    Route::get('/procurement/supplier', [ProcurementController::class, 'supplier'])->name('procurement.supplier');
 
+
+    /////////////////////////////////////////// SUPPLIER //////////////////////////////////////////
     Route::post('/procurement/supplier/add-supplier', [SupplierController::class,'storeSupplier']);
     Route::get('/procurement/supplier/get-supplier', [SupplierController::class,'getSupplier']);
-    Route::get('/procurement/supplier/get-active-supplier', [SupplierController::class,'getActiveSupplier']);
+
+    /////////////////////////////////////////////// CREATE PO /////////////////////////////////////////////////////
+    Route::get('/procurement/create-purchase-order/get-active-supplier', [SupplierController::class,'getActiveSupplier']);
+    Route::get('/procurement/create-purchase-order/get-categories', [PurchaseOrderController::class,'getCategories']);
+    Route::get('/procurement/create-purchase-order/get-items', [PurchaseOrderController::class,'getItems']);
 });
