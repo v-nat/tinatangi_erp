30d345763ee7c1095dddf3ccacc771a888fab230
Nathaniel <nathanielvasquez329@gmail.com>
2025-10-08 12:13:00 +0800

    latest employee attendance amendments

--- resources/views/layouts/modals/attendance-modal.blade.php
+++ resources/views/layouts/modals/attendance-modal.blade.php
@@ -15,17 +15,6 @@
                                 <input type="hidden" name="employee_id" id="employeeIdInput"
                                     value="{{ Auth::user()->id ?? '' }}">
 
-                                <div class="row d-block mb-3">
-                                    <div class="col-12 w-100">
-                                        <button type="button" class="btn btn-lg btn-success col-12" id="timeInBtn">Time
-                                            In</button>
-                                    </div>
-                                    <div class="col-12 w-100 mt-1">
-                                        <button type="button" class="btn btn-lg btn-danger col-12"
-                                            id="timeOutBtn">TimeOut</button>
-                                    </div>
-                                </div>
-
                                 <p class="mt-4 fs-5">
                                     <span class="d-block d-md-inline me-md-5">
                                         Time In: <span id="timeInDisplay" class="text-success">--:--:--</span>
@@ -39,6 +28,17 @@
                                 <p class="text-muted fs-6 mt-2">
                                     Minutes Today: <span id="totalHours" class="text-primary">0 minutes</span>
                                 </p>
+
+                                <div class="row d-block mb-3">
+                                    <div class="col-12 w-100">
+                                        <button type="button" class="btn btn-lg btn-success col-12" id="timeInBtn">Time
+                                            In</button>
+                                    </div>
+                                    <div class="col-12 w-100 mt-1">
+                                        <button type="button" class="btn btn-lg btn-danger col-12"
+                                            id="timeOutBtn">TimeOut</button>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
@@ -50,32 +50,3 @@
         </div>
     </div>
 </div>
-
-<style>
-    @media (max-width: 576px) {
-        #realtimeClock {
-            font-size: 2rem;
-            text-align: center;
-        }
-
-        .time-record {
-            font-size: 16px;
-            text-align: center;
-        }
-
-        .btn-lg {
-            width: 100%;
-            margin-bottom: 10px;
-        }
-
-        .card {
-            padding: 15px;
-            margin-bottom: 1rem;
-        }
-
-        .card-header {
-            padding: 0.75rem 1rem;
-            text-align: center;
-        }
-    }
-</style>
