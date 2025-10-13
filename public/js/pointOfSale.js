918e319b0651994a01d9a7419a08f56a6f72fef9
Nathaniel <nathanielvasquez329@gmail.com>
2025-10-13 23:36:29 +0800

    fix exit pos

--- public/js/pointOfSale.js
+++ public/js/pointOfSale.js
@@ -1,23 +1,20 @@
 $(document).ready(function () {
-
-    
-});
-
-$(document).on("click", "#exit-pos-btn", function (e) {
-    e.preventDefault();
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You are about End POS Session.",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Exit",
-    }).then((result) => {
-        if (!result.isConfirmed) {
-            return;
-        }
-        $("#LoadingScreen").fadeIn(200);
-        window.location.href = "/operations";
+    $(document).on("click", "#exit-pos-btn", function (e) {
+        e.preventDefault();
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You are about End POS Session.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Exit",
+        }).then((result) => {
+            if (!result.isConfirmed) {
+                return;
+            }
+            $("#LoadingScreen").fadeIn(200);
+            window.location.href = "/operations";
+        });
     });
 });
--- resources/views/pages/admin/operations/point-of-sales.blade.php
+++ resources/views/pages/admin/operations/point-of-sales.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.modules-layouts.pos-layout')
+@section('title') Point of Sale System | Tinatangi Cafe @endsection
 @section('content')
-    
+
 @endsection
 @section('scripts')
     <script type="module" src="{{ asset('js/pointOfSale.js') }}   "></script>
