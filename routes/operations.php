e86e713fdfb1258cb552cf950ea4e9ca433c5f65
Nathaniel <nathanielvasquez329@gmail.com>
2025-10-13 18:21:05 +0800

    a

--- routes/operations.php
+++ routes/operations.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Operations\OperationsController;
+
 
 Route::middleware(['auth' , 'isEmployee'])->group(function () {
     ////////////////////////////////// DASHBOARD ////////////////////////////////////////////////
-    Route::get('/inventory', [OperationsController::class, 'index'])->name('op.dashboard');
+    Route::get('/operations', [OperationsController::class, 'index'])->name('op.dashboard');
 });
