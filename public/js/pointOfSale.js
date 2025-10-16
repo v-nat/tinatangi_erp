67ea846dfc26f62770cb6a7534be936924bf979e
Nathaniel <nathanielvasquez329@gmail.com>
2025-10-16 08:57:31 +0800

    a

--- public/js/pointOfSale.js
+++ public/js/pointOfSale.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
                     let imageUrl;
 
                     if (imagePath && imagePath !== "N/A") {
-                        imageUrl = "/storage/" + imagePath;
+                        imageUrl = "/" + imagePath;
                     } else {
                         imageUrl = DEFAULT_PRODUCT_IMAGE;
                     }
