f77025eebb31a9150c23e5f427678540ad2f1fb7
Nathaniel <nathanielvasquez329@gmail.com>
2025-10-17 11:54:42 +0800

    a

--- app/Http/Controllers/Admin/Inventory/ProductController.php
+++ app/Http/Controllers/Admin/Inventory/ProductController.php
@@ -173,7 +173,7 @@ class ProductController extends Controller
 
         $servings = ($minServings == PHP_INT_MAX) ? 0 : $minServings;
 
-        if ($servings == 0 && $product->status != 2) {
+        if (($servings / 1000) == 0 && $product->status != 2) {
             $product->status = 2;
             $product->save();
         }
