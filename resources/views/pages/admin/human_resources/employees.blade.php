52235f3c59e41088f2c6386e84110f37f0685d61
Nathaniel <nathanielvasquez329@gmail.com>
2025-10-23 23:10:03 +0800

    tbl employees

--- resources/views/pages/admin/human_resources/employees.blade.php
+++ resources/views/pages/admin/human_resources/employees.blade.php
@@ -47,7 +47,7 @@
             </div>
             <div class="card-body row">
 
-                <div class="table-responsive col-12">
+                <div class="table-responsive d-flex col-12">
                     <table id="employee_table" class="table table-hover dataTable no-footer"
                         style="width:100% !important; table-layout:fixed">
                         <thead>
