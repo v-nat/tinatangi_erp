e385aaa55f9dae4326eb8b76ac67914f88d7a222
Nathaniel <nathanielvasquez329@gmail.com>
2025-10-29 16:38:30 +0800

    kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,11 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-
-        // ADD THIS LINE
-        $schedule->command('test:cron')->everyMinute();
-        // $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
+        $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
     }
 
     /**
