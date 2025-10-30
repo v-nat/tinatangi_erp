87d43870c68f1221dbb59ba270ba6f655db6d7bd
Nathaniel <nathanielvasquez329@gmail.com>
2025-10-30 18:41:12 +0800

    login title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/font/Nunito/static/stylesheet.css')}}">
-    <title>Login - Tinatangi ERP</title>
+    <title>Login - Tinatangi Cafe ERP Management System</title>
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/css/app.css">
