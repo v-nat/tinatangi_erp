0dabb8404a367bac77acd5bea2f455aa5c4cfba1
Nathaniel <nathanielvasquez329@gmail.com>
2025-11-04 21:55:08 +0800

    finance

--- resources/views/pages/admin/finance/index.blade.php
+++ resources/views/pages/admin/finance/index.blade.php
@@ -8,7 +8,7 @@
 
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('finance.dashboard') }}">Finance</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('finance') }}">Finance</a></li>
             <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
         </ol>
     </nav>
