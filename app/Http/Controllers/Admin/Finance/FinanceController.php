c863c7a80c4476a99d1d974a913c41663fbbaf42
Nathaniel <nathanielvasquez329@gmail.com>
2025-11-04 22:02:24 +0800

    fix

--- app/Http/Controllers/Admin/Finance/FinanceController.php
+++ app/Http/Controllers/Admin/Finance/FinanceController.php
@@ -52,7 +52,7 @@ class FinanceController extends Controller
 
         $payrollData = DB::table('payrolls')
             ->join('employees', 'payrolls.employee_id', '=', 'employees.id')
-            ->join('departments', 'employees.department_id', '=', 'departments.id')
+            ->join('departments', 'employees.department', '=', 'departments.id')
             ->select('departments.name', DB::raw('COALESCE(SUM(payrolls.gross_pay), 0) as total_payroll'))
             ->where('payrolls.status', '!=', 11)
             ->groupBy('departments.name')
