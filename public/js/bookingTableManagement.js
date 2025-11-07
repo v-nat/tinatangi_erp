e7acafe20d01e34aec3715f44a0541db77c250ae
Nathaniel <nathanielvasquez329@gmail.com>
2025-11-08 00:38:28 +0800

    time

--- public/js/bookingTableManagement.js
+++ public/js/bookingTableManagement.js
@@ -29,6 +29,9 @@ $(document).ready(function () {
             },
             {
                 data: "time",
+                render: function (data, type, row) {
+                    return data ? formatTime(data) : "N/A";
+                },
 
             },
             { data: "people" },
